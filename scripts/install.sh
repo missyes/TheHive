492dc59597a0fdc07365841a5efd334cb9c101e0
missyes <akk.alexs1997@gmail.com>
2022-08-29 17:11:34 +1000

    auto build

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ folders="/etc/thehive /opt/thehive"
 files="/usr/lib/systemct"
 #folders="/etc/default/thehive /etc/thehive /opt/thehive /usr/bin/cloner /usr/bin/migrate /usr/bin/thehive /usr/lib/systemd/system/thehive.service /usr/share/doc/thehive/copyright"
 backup () {
+	echo -e "\033[0;32mBackuping...\033[m"
 	if [ ! -d "./backups" ]; then
 		mkdir ./backups
 	fi
@@ -27,18 +28,25 @@ restore () {
 	systemctl start thehive
 }
 install (){
+	echo -e "\033[0;32mBuilding...\033[m"
+	/bin/bash ./build.sh r
+	backup
+	echo -e "\033[0;32mRestart elastic (drops after build)\033[m"
 	systemctl restart elasticsearch
 	if [ ! -d /opt/thehive ];then
+		echo -e "\033[0;32mPostinstaltaion\033[m"
 		bash ../package/debian/postinst configure
 	fi
 	systemctl stop thehive
 	systemctl stop cassandra
+	echo -e "\033[0;32mUpdatig\033[m"
 	cp -R ../target/universal/stage/bin /opt/thehive/bin
 	cp -R ../target/universal/stage/lib /opt/thehive/lib
 	cp ../package/thehive.service /lib/systemd/system/
 	systemctl daemon-reload
 	systemctl start cassandra
 	systemctl start thehive
+	echo -e "\033[0;32mAll services up\033[m"
 }
 case "$1" in
 	backup)
