182d04f8628315e9603e0ddb35501620767d64fd
missyes <34905685+missyes@users.noreply.github.com>
2022-08-29 16:04:18 +1000

    Update scdecoder.js

--- frontend/app/scripts/filters/scdecoder.js
+++ frontend/app/scripts/filters/scdecoder.js
@@ -1,12 +1,12 @@
 (function() {
     'use strict';
     angular.module('theHiveFilters').filter('scdecoder', function() {
-	return function(value){
-	    if(!value) {
-		return '';
+        return function(value){
+            if(!value) {
+                return '';
 	    }
-	    var txt = document.createElement("textarea");
-	    txt.innerHTML = value;
+        var txt = document.createElement("textarea");
+        txt.innerHTML = value;
 	    return txt.value;
 	    //return value.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#x27;/g, '\'').replace(/&#x2F;/g, '/');
 	};
